05ee7bc56d5eb61cc70abdd4d29eba8b3a92cd15
Kerry Burns <30881654+kburns17@users.noreply.github.com>
2018-04-03 12:54:12 -0500

    Update joins-solution.sql

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,1 +1,18 @@
+--1. Get all customers and their addresses.
+SELECT * FROM "customers" JOIN "addresses" ON "customers"."id" = "addresses"."id";
 
+--2. Get all orders and their line items.
+SELECT * FROM "orders" JOIN "line_items" ON "orders"."id" = "line_items"."order_id";
+
+--3. Which warehouses have cheetos?
+SELECT * FROM "warehouse" JOIN "warehouse_product" ON "warehouse"."id" = "warehouse_product"."warehouse_id"  WHERE "product_id" = 5;
+
+--4. Which warehouses have diet pepsi?
+
+--5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+
+--6. How many customers do we have?
+
+--7. How many products do we carry?
+
+--8. What is the total available on-hand quantity of diet pepsi?
